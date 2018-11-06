595b8562efbb8dd74038b3326c8eedb4e3660afe
Chris Lion <christhopherleao@icloud.com>
2018-11-06 15:14:00 -0200

    tweaking wave component states

--- src/components/Wave/Wave.jsx
+++ src/components/Wave/Wave.jsx
@@ -1,5 +1,7 @@
 import React, { PureComponent } from 'react';
 import './Wave.scss';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faMusic } from '@fortawesome/free-solid-svg-icons';
 import { ReactComponent as WaveSVG } from './wave.svg';
 
 const etapas = ['Sample Step 1', 'Sample Step 2', 'Sample Step 3'];
@@ -7,43 +9,51 @@ const etapas = ['Sample Step 1', 'Sample Step 2', 'Sample Step 3'];
 class Wave extends PureComponent {
   constructor(props) {
     super(props);
-    this.state = {};
+    this.state = {
+      selectedItem: 0
+    };
+  }
+
+  showDetails(i) {
+    this.setState({
+      selectedItem: i + 1
+    });
   }
 
   render() {
+    const { selectedItem } = this.state;
+
     return (
       <section id="Wave" className="wave-section">
-        <div className="wave-btn-container">
+        <div className={`wave-btn-container ${selectedItem}`}>
           <WaveSVG />
 
-          <ul className="tabs justify-content-center">
+          <ul className="tabs">
             {etapas.map((item, i) => (
-              <li key={item} data-id={i}>
-                <button type="button" className="button wave-btn">
-                  <span className="caption">{item}</span>
-                </button>
-              </li>
+              <button
+                key={item}
+                type="button"
+                onClick={() => this.showDetails(i)}
+                className={`row button wave-btn ${selectedItem === i + 1}`}
+                align="center center"
+              >
+                <FontAwesomeIcon icon={faMusic} className="caption" />
+              </button>
             ))}
           </ul>
         </div>
 
-        <ul className="row tab__content">
-          <li className="active">
-            <div className="content__wrapper" />
-          </li>
-
-          {etapas.map(item => (
-            <li key={item} className="offset-md-3 col-md-6">
-              <div className="content__wrapper text-center">
-                <h2>{item}</h2>
-                <p>
-                  Lorem ipsum dolor sit amet consectetur adipisicing elit.
-                  Aliquid optio assumenda praesentium doloremque asperiores
-                  reiciendis temporibus, et repudiandae dolor doloribus libero
-                  ad est! Exercitationem voluptatum, architecto sed placeat
-                  aliquam excepturi.
-                </p>
-              </div>
+        <ul className="wave-details" contain="">
+          {etapas.map((item, i) => (
+            <li className={`center ${selectedItem === i + 1}`} key={item}>
+              <h3>{item}</h3>
+              <p>
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. Aliquid
+                optio assumenda praesentium doloremque asperiores reiciendis
+                temporibus, et repudiandae dolor doloribus libero ad est!
+                Exercitationem voluptatum, architecto sed placeat aliquam
+                excepturi.
+              </p>
             </li>
           ))}
         </ul>
