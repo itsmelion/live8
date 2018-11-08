dbacd1eb775a0265c3bec4504d052f70ce6ba6f2
Chris Lion <christhopherleao@icloud.com>
2018-11-08 20:32:47 -0200

    Virtual reality pownd

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,11 +1,12 @@
 import React from 'react';
 import './Header.scss';
 
-const Header = ({ title, subtitle, classe }) => (
-  <header column="" align="center center" className={`Header ${classe}`}>
-    <div className="center">
-      <h1>{title}</h1>
+const Header = ({ id, title, subtitle, classe, paragraph }) => (
+  <header id={id} align="center center" className={`column Header ${classe}`}>
+    <div className="contain center">
+      <h1 className="display">{title}</h1>
       <h3>{subtitle}</h3>
+      {paragraph && <p className="mt1">{paragraph}</p>}
     </div>
   </header>
 );
--- src/components/VR/VR.jsx
+++ src/components/VR/VR.jsx
@@ -1,10 +1,52 @@
 import React from 'react';
+import Combo from '../Combo/Combo';
+import Header from '../Header/Header';
+import AppleCards from '../AppleCards/AppleCards';
+import vr1 from './images/1.png';
+import vr2 from './images/2.png';
 import './VR.scss';
 
 const VR = () => (
-  <section id="VR">
-    {}
-  </section>
+  <>
+    <Header
+      id="headerVR"
+      title="Virtual Reality"
+      subtitle="Facilis reiciendis incidunt quae repellendus cumque"
+      paragraph="Lorem ipsum dolor sit amet consectetur adipisicing elit.
+      Facilis reiciendis incidunt quae repellendus cumque.
+      Molestiae doloribus provident voluptatibus necessitatibus sunt,
+      distinctio, corrupti, laboriosam libero optio impedit
+      soluta sint commodi assumenda."
+    />
+
+    <section className="contain center" id="insight360">
+      <div className="mt2">
+        <h1>Section Title</h1>
+        <p className="mv1">
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Facilis
+          reiciendis incidunt quae repellendus cumque. Molestiae doloribus
+          provident voluptatibus necessitatibus sunt, distinctio, corrupti,
+          laboriosam libero optio impedit soluta sint commodi assumenda.
+        </p>
+      </div>
+
+      <div contain="">
+        <img id="box-closed" src={vr1} alt="oculos Live8 atributos" />
+        <img id="box-open" src={vr2} alt="oculos Live8 atributos" />
+      </div>
+
+      <div className="mv10">
+        <div className="mv10">
+          <h2>Exemplos</h2>
+          <p>Veja abaixo alguns vídeos 360º</p>
+        </div>
+
+        <AppleCards />
+      </div>
+    </section>
+
+    <Combo />
+  </>
 );
 
 export default VR;
